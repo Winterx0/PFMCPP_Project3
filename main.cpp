17738792a98a53e34ccb64778ca35b11df8c137d
Melissa Hurley <winterhurley@gmail.com>
2020-01-12 07:11:06 +0000

    Added constructors for User-Defined types and printed data

--- main.cpp
+++ main.cpp
@@ -124,32 +124,44 @@ struct GuitarChords
 {
     int tone;
     int numStrings = 12;
-    double volumeKnob = 2;
     double getNumFrets = 2.0;
     unsigned int lengthOfNeck;
     
     struct Guitar
     {
         int steelNeck;
+        float inTune { 440.0f };
+        float tuneKey { 4.0f };
         bool isElectricGuitar = true;
-        float inTune = 440.0f;
+        
+        Guitar() { steelNeck = 0; }
+        void printGuitar() 
+        {
+            std::cout << "Guitar " << "Key: " << steelNeck + tuneKey << "  " << "Hz: " << inTune << std::endl;
+        }
     };
 
-    void tuneAndPlayGuitar(int steelNeck, bool isElectricGuitar, float inTune);
+    GuitarChords() { tone = 0; }
+    void printGuitarChords()
+    {
+        std::cout << "Guitar " << tone + numStrings << " String Ibanez" << std::endl;
+    }
+
+    void tuneAndPlayGuitar(int steelNeck, bool isElectricGuitar, float inTune, float tuneKey);
 
     int ready;
     int play();
 };
 
-void GuitarChords::tuneAndPlayGuitar(int steelNeck, bool isElectricGuitar, float inTune)
+void GuitarChords::tuneAndPlayGuitar(int steelNeck, bool isElectricGuitar, float inTune, float tuneKey)
 {
     if(isElectricGuitar)
     {
-        lengthOfNeck *= numStrings + volumeKnob * tone + ready / getNumFrets * 0.5;
+        lengthOfNeck *= numStrings * tone + ready * getNumFrets * 0.5;
     }
     else
     {
-        steelNeck *=+ lengthOfNeck + inTune + 2;
+        steelNeck *= lengthOfNeck + inTune * tuneKey + 2;
     }
 }
 
@@ -171,9 +183,22 @@ struct Component
 
     struct DCUnit
     {
+        int box;
+        float hasCable { 3.0f };
         bool connectsToComputer = true;
-        float hasCable = 3.0f;
+        
+        DCUnit() { box = 0; }
+        void printDCUnit()
+        {
+            std::cout << "DC Unit " << box + hasCable << " USB" << std::endl;
+        }
     };
+    
+    Component() { current = 0; }
+    void printComponent()
+    {
+        std::cout << "Component " << current + isVoltage << " watts"<< std::endl;
+    }
 
     void plugIn( bool connectsToComputer, float hasCable);
 
@@ -211,9 +236,22 @@ struct NeuroScience
 
     struct NeuroPath
     {
+        int neuroPlasticity;
+        float genericThought { 500.0f };
         bool isSmart = false;
-        float genericThought = 500.0f;
+        
+        NeuroPath() { neuroPlasticity = 0; }
+        void printNeuroPath()
+        {
+            std::cout << "Neuro Path " << neuroPlasticity + genericThought << "% Cognitive Function" << std::endl;
+        }
     };
+    
+    NeuroScience() { neurons = 0; }
+    void printNeuroScience()
+    {
+        std::cout << "Neuro Science " << neurons + brainScience << " Neurons" << std::endl;
+    }
 
     void sleep(bool isSmart, float genericThought);
 
@@ -245,28 +283,43 @@ int NeuroScience::cellCount()
  */
 struct DogBreed
 {
+    int pupper;
     int pitBull = 1;
     int terrier = 5;
-    double mixedBreed = 5.0;
-    float bigDog = 1.7f;
     unsigned int age = 0;
+    double mixedBreed = 5.0;
+    float bigDog { 1.7f };
 
     struct Dog
     {
+        int manyPuppies;
+        float litter { 100.0f };
         bool isApuppy = false;
-        float litter = 100.0f;
+        
+        Dog() { manyPuppies = 0; }
+        void printDog()
+        {
+            std::cout << "Dog " << manyPuppies + litter << "% Chihuahua" << std::endl;
+        }    
     };
+    
+    DogBreed() { pupper = 0; }
+    void printDogBreed()
+    {
+        std::cout << "Dog Breed " << pupper + pitBull * mixedBreed << " puppies" << std::endl;
+    }
 
     void walkDog( bool isApuppy, float litter );
 
     int barks();
+    int woof;
 };
 
 void DogBreed::walkDog(bool isApuppy, float litter)
 {
     if(isApuppy)
     {
-        age *= pitBull + mixedBreed;
+        age *= pitBull + mixedBreed * pupper + woof;
     }
     else
     {
@@ -287,16 +340,29 @@ struct Hammer
     int slam = 333;
     double swing = 2.0;
     double hit = 2.6;
-    float steel = 13.3f;
-    float sledge = 77.0f;
+    float steel { 13.3f };
+    float sledge { 77.0f };
     unsigned int force = 0;
     unsigned int heavyDuty;
 
     struct HammerOn
     {
+        int ham;
+        float master { 0.0f };
         bool isAshredder = true;
-        float master = 0.0f;
+        
+        HammerOn() { ham = 0; }             //3) the constructor
+        void printHammerOn()            //1) the member function
+        {
+            std::cout << "Hammer On " << ham + 100 << "% Man O War" << std::endl;      //5) printing out something interesting
+        }    
     };
+    
+    Hammer() { heavyDuty = 0; }             //3) the constructor
+    void printHammer()            //1) the member function
+    {
+        std::cout << "Hammmer " << heavyDuty + sledge * steel / slam << " Hz of Thunder"<< std::endl;  //5) printing out something interesting
+    }
 
     void always( bool isAshredder, float master );
 
@@ -307,10 +373,19 @@ struct Hammer
         int blocks;
         int nails = 10;
         double screws = 2.2;
-        float electricHammer = 440.0f;
-    };
+        double staples = 1.0;
+        float electricHammer { 440.0f };
+        
+        ToolType() { blocks = 0; }
+        void printToolType()
+        {
+            std::cout << "Tool Type " << blocks + nails << " nails" << std::endl; 
+        }
+        
+        int bricks;
+    };   
 
-    void notEnoughTools(int blocks, int nails, double screws, float electricHammer);
+    void notEnoughTools(int blocks, int nails, double staples, double screws, float electricHammer);
 
     int breakIt();
 };
@@ -334,11 +409,11 @@ int Hammer::build()
     return {};
 }
 
-void Hammer::notEnoughTools(int blocks, int nails, double screws, float electricHammer)
+void Hammer::notEnoughTools(int blocks, int nails, double staples, double screws, float electricHammer)
 {
     if(bool hammerHits = true)
     {
-        blocks *= nails + screws;
+        blocks *= nails + screws * staples;
     }
     else
     {
@@ -358,25 +433,38 @@ struct TreeComponent
     int test;
     int value = 5000000;
     int tree = 10000000;
-    float purposeFunction = 808.1f;
+    float purposeFunction { 808.1f };
     double process = 4.2;
 
     struct Configure
     {
         int nestTest;
         int classWithin  = 1;
+        float submit { 14.0f };
         bool treeIsBuilt = false;
-        float submit = 14.0f;
+        
+        Configure() { nestTest = 0; }             //3) the constructor
+        void printConfigure()            //1) the member function
+        {
+            std::cout << "Configure " << nestTest + classWithin * submit << " arrays" << std::endl;  //5) printing out something interesting
+        }    
+        
     };
+    
+    TreeComponent() { test = 0; }
+    void printTreeComponent()
+    {
+        std::cout << "Tree Component " << test + value * tree << std::endl;
+    }    
 
-    void comp( int nestTest, int classWithin, bool treeIsBuilt,  float submit);
+    void structure( int nestTest, int classWithin, bool treeIsBuilt,  float submit);
 
     int valueTree;
     int internally;
     int setUp();
 };
 
-void TreeComponent::comp( int nestTest, int classWithin, bool treeIsBuilt,  float submit)
+void TreeComponent::structure( int nestTest, int classWithin, bool treeIsBuilt,  float submit)
 {
     if(treeIsBuilt)
     {
@@ -400,8 +488,8 @@ int TreeComponent::setUp()
  */
 struct MainApp
 {
-    float blackBox = 3.0f;
-    float localNetwork = 2.0f;
+    float blackBox { 3.0f };
+    float localNetwork { 2.0f };
     unsigned int webBased = true;
     int manuals = 3;
     int byteFill;
@@ -409,9 +497,23 @@ struct MainApp
 
     struct DBG
     {
-        bool menuWorks = true;
+        int d;
         float generate(int localWeb = 0.0, double staticMem = 15.45);
+        bool menuWorks = true;
+        
+        DBG() { d = 0; }
+        void printDBG()
+        {
+            std::cout << "DBG " << d + 1 << " error" << std::endl;
+        }
+        
     };
+    
+    MainApp() { reTry = 0; }
+    void printMainApp()
+    {
+        std::cout << "Main App " << reTry + manuals * blackBox * localNetwork << " MB" << std::endl;
+    }    
 
     void crashing(bool menuWorks, float generate);
 
@@ -441,23 +543,39 @@ int MainApp::newest()
  */
 struct NewUnderstanding
 {
+    int x = 25;
+    int y = 25;
+    int w;
+    int z();
+    
     char time = 'N';
     char to = 'E';
     char build = 'W';
+    char the = '~';
     char block = '!';
-
-    int x = 25;
-    int y = 25;
-    int z();
 
     struct TestFunctions
     {
+        int t;
         float multiply(int x = 25, int y = 25);
-        float add(char time, char to, char build, char block);
+        float add(char time, char to, char build, char the, char block);
         bool compilesCorrect = true;
+        
+        TestFunctions() { t = 0; }
+        void printTestFunctions()
+        {
+            std::cout << "Test Functions " << t << "% accuracy" << std::endl;
+        }    
     };
+    
+    NewUnderstanding() { w = 0; }
+    void printNewUnderstanding()
+    {
+        std::cout << "New Understanding " << w + x * y << "%" << std::endl;
+    }
 
     void overloading(float multiply, float add, bool compilesCorrect);
+    
 };
 
 void NewUnderstanding::overloading(float multiply, float add, bool compilesCorrect)
@@ -487,10 +605,23 @@ struct NewPlug
     float purchase(int number = 1, double integer = 2.0);
 
     struct BasicDesign
-    {
-        bool hasAdapter = false;
+    { 
         int hazard;
+        int b;
+        bool hasAdapter = false;
+        
+        BasicDesign() { b = 0; }
+        void printBasicDesign()
+        {
+            std::cout << "Basic Design " << b + 2 << " sockets" << std::endl;
+        }       
     };
+    
+    NewPlug() { toolUse = 0; }
+    void printNewPlug()
+    {
+        std::cout << "New Plug " << toolUse + getTools << " watts" << std::endl;
+    }
 
     void safteyUse(bool hasAdapter, int hazard, float purchase);
     int workFlow();
@@ -507,7 +638,7 @@ void NewPlug::safteyUse(bool hasAdapter, int hazard, float purchase)
         hazard *= getTools * generics;
     }
 
-    toolUse *= getTools * generics * purchase + 1;
+    toolUse *= getTools * generics * purchase + 3;
 }
 
 int NewPlug::workFlow()
@@ -527,10 +658,25 @@ struct DidItBreak
 
     struct Learn
     {
+        int l;
         bool brokenCompiler = true;
         bool isLearning = true;
         float crazyStuff(double check = 1.0, double status = 500.1, double wonder = 30.3, int val = 44, int UDT = 77);
+        
+        Learn() { l = 0; }
+        void printLearn()
+        {
+            std::cout << "Learn " << l + 10 << " new things" << std::endl;
+        }        
+        
+        short knowledge;
     };
+    
+    DidItBreak() { makeHappen = 0; }
+    void printDidItBreak()
+    {
+        std::cout << "Did It Break " << makeHappen + 100 << "% did" << std::endl;
+    }    
 
     void doubleFunction(bool brokenCompiler, float unknown);
     void checkStatus(bool isLearning, float crazyStuff);
@@ -570,6 +716,68 @@ int DidItBreak::needThings()
 #include <iostream>
 int main()
 {
-	Example::main();
-    std::cout << "good to go!" << std::endl;
+    std::cout << "Print:" << std::endl;
+    
+    GuitarChords guitarChords;
+    guitarChords.printGuitarChords();
+    
+    GuitarChords::Guitar guitar;
+    guitar.printGuitar();
+    
+    Component component;
+    component.printComponent();
+    
+    Component::DCUnit dcunit;
+    dcunit.printDCUnit();
+    
+    NeuroScience neuroScience;
+    neuroScience.printNeuroScience();
+    
+    NeuroScience::NeuroPath neuroPath;
+    neuroPath.printNeuroPath();
+    
+    DogBreed dogBreed;
+    dogBreed.printDogBreed();
+    
+    DogBreed::Dog dog;
+    dog.printDog();
+    
+    Hammer hammer;
+    hammer.printHammer();
+    
+    Hammer::HammerOn hammerOn;
+    hammerOn.printHammerOn();
+    
+    Hammer::ToolType toolType;
+    toolType.printToolType();
+    
+    TreeComponent treeComponent;
+    treeComponent.printTreeComponent();
+    
+    TreeComponent::Configure configure;
+    configure.printConfigure();
+    
+    MainApp mainApp;
+    mainApp.printMainApp();
+    
+    MainApp::DBG dbg;
+    dbg.printDBG();
+    
+    NewUnderstanding newUnderstanding;
+    newUnderstanding.printNewUnderstanding();
+    
+    NewUnderstanding::TestFunctions testFunctions;
+    testFunctions.printTestFunctions();
+    
+    NewPlug newPlug;
+    newPlug.printNewPlug();
+    
+    NewPlug::BasicDesign basicDesign;
+    basicDesign.printBasicDesign();
+    
+    DidItBreak didItBreak;
+    didItBreak.printDidItBreak();
+    
+    DidItBreak::Learn learn;
+    learn.printLearn();
 }
